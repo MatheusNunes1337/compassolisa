548b49435a09bcb6db0be8429108c44e620a810a
Matheus <matheusyagami147@gmail.com>
2021-10-20 20:49:36 -0300

    feat(index): set the initial setup in index.js

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+
+class App {
+    constructor() {
+        this.express = express()
+        this.middlewares()
+    }
+
+    middlewares() {
+        this.express.use(express.json())
+    }
+
+}
+
+module.exports = new App().express
