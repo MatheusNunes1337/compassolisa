94f2460287c5f87ab3e6d0dd255f726b373e2980
Matheus <matheusyagami147@gmail.com>
2021-10-20 21:34:57 -0300

    feat(src/index): add errorHandler into class App

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
 const express = require('express')
 
+const errorHandler = require('./app/middlewares/errorHandler')
 const dbConnection = require('./infra/database')
+
 dbConnection.connect()
 
 class App {
     constructor() {
         this.express = express()
         this.middlewares()
+        this.errors()
     }
 
     middlewares() {
         this.express.use(express.json())
+    }
+
+    errors() {
+        this.express.use(errorHandler)
     }
 
 }
