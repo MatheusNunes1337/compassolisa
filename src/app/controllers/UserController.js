9f0481db4f2a87b26a2e99b57da3bb3a3c37bbb0
Matheus <matheusyagami147@gmail.com>
2021-10-20 22:10:10 -0300

    feat(UserController): implement the getById method

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,9 @@
+const UserService = require('../services/UserService')
+
+class UserController {
+    async getById(req, res) {
+        const { id } = req.params.id 
+        const user = await UserService.findById(id)
+        return res.status(200).json(user)
+    }
+}
