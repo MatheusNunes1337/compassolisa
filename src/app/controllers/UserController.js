37c1b747d5bbcf2df2c448281f653080c7d6ffdb
Matheus <matheusyagami147@gmail.com>
2021-10-20 23:21:51 -0300

    fix(UserController): add try and catch in the getById method

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,10 +1,14 @@
 const UserService = require('../services/UserService')
 
 class UserController {
-    async getById(req, res) {
-        const { id } = req.params.id 
-        const user = await UserService.findById(id)
-        return res.status(200).json(user)
+    async getById(req, res, next) {
+        try {
+            const { id } = req.params.id 
+            const user = await UserService.findById(id)
+            return res.status(200).json(user)
+        } catch(err) {
+            next(err)
+        }
     }
 }
 
