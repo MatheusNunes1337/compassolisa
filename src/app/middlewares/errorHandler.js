5946bedfdd07710d98e86584cb8636a4880ddd77
Matheus <matheusyagami147@gmail.com>
2021-10-20 23:11:45 -0300

    fix: add next as parameter in errorHandler middleware

--- src/app/middlewares/errorHandler.js
+++ src/app/middlewares/errorHandler.js
@@ -1,12 +1,14 @@
 const NotFound = require('../errors/NotFound')
 
-const errorHandler = (err, req, res) => {
+const errorHandler = (err, req, res, next) => {
+
     let statusCode = 500
+    
+    if(err instanceof Error)
+        statusCode = 400
 
     if(err instanceof NotFound) 
         statusCode = 404
-    if(err instanceof Error)
-        statusCode = 400
         
     res.status(statusCode).json({error: err.message})    
 }
