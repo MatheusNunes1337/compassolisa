df50f0493d5b21c23eb039d5ff46f4a99c4fe8f6
Matheus <matheusyagami147@gmail.com>
2021-10-21 12:52:47 -0300

    feat(routes/user.router): implement the post endpoint to create a new user

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -2,5 +2,6 @@ const userRouter = require('express').Router()
 const UserController = require('../app/controllers/UserController')
 
 userRouter.get('/:id', UserController.getById)
+userRouter.post('/', UserController.create)
 
 module.exports = userRouter
