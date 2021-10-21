2f3819e10c65f2f30db8db04b6f5c1a5126259c4
Matheus <matheusyagami147@gmail.com>
2021-10-21 15:15:51 -0300

    feat(UserController): create the getAll method

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,6 +1,16 @@
 const UserService = require('../services/UserService')
 
 class UserController {
+    async getAll(req, res, next) {
+        try {
+            const users = await UserService.findAll()
+            return res.status(200).json(users)
+        } catch(err) {
+            next(err)
+        }
+    }
+
+
     async getById(req, res, next) {
         try {
             const { id } = req.params 
