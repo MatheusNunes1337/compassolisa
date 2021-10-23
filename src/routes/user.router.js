7608b93ad516d2ae40a08d878b25ea109d84d853
Matheus <matheusyagami147@gmail.com>
2021-10-22 21:15:08 -0300

    feat(routes/user.router): add user validation in post endpoint

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,9 +1,10 @@
 const userRouter = require('express').Router()
 const UserController = require('../app/controllers/UserController')
+const createValidation = require('../app/validations/user/create')
 
 userRouter.get('/', UserController.getAll)
 userRouter.get('/:id', UserController.getById)
-userRouter.post('/', UserController.create)
+userRouter.post('/', createValidation, UserController.create)
 userRouter.put('/:id', UserController.update)
 userRouter.delete('/:id', UserController.delete)
 
