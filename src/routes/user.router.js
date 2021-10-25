88570ee9049375b4b011b903037114043af89d75
Matheus <matheusyagami147@gmail.com>
2021-10-24 22:36:26 -0300

    feat(routes/user.router): add createIdFormat in getById endpoint

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,9 +1,10 @@
 const userRouter = require('express').Router()
 const UserController = require('../app/controllers/UserController')
 const createValidation = require('../app/validations/user/create')
+const checkIdFormat = require('../app/middlewares/checkIdFormat')
 
 userRouter.get('/', UserController.getAll)
-userRouter.get('/:id', UserController.getById)
+userRouter.get('/:id', checkIdFormat, UserController.getById)
 userRouter.post('/', createValidation, UserController.create)
 userRouter.put('/:id', UserController.update)
 userRouter.delete('/:id', UserController.delete)
