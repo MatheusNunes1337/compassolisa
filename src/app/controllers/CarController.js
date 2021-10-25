527e0e6680c027e422eb423766c6b3ddbbf0bb72
Matheus <matheusyagami147@gmail.com>
2021-10-25 08:30:22 -0300

    feat(controllers): create the getAll method in CarController

---
+++ src/app/controllers/CarController.js
@@ -0,0 +1,26 @@
+const CarService = require('../services/CarService')
+
+class CarController {
+    async getAll(req, res, next) {
+        try {
+            let cars
+        
+            req.query.offset = parseInt(req.query.offset)
+            req.query.limit = parseInt(req.query.limit)
+        
+            if(Object.keys(req.query).length == 2) {
+                cars = await UserService.findAll(req.query)
+            }
+            /*    
+            else {
+                cars = await UserService.findAll(req.query)
+            }
+            */
+            return res.status(200).json(cars)
+        } catch(err) {
+            next(err)
+        }
+    }
+}
+
+module.exports = new CarController()
