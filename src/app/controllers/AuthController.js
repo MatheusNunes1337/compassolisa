73f55ac7b017d3b921723231288b77621fa8bc34
Matheus <matheusyagami147@gmail.com>
2021-10-24 23:01:41 -0300

    feat(AuthController): create the login method

---
+++ src/app/controllers/AuthController.js
@@ -0,0 +1,14 @@
+const AuthService = require('../services/AuthService')
+
+class AuthController {
+    async login(req, res, next) {
+        try {
+            const response = AuthService.login(req.body)
+            return res.status(201).json(response)
+        } catch(err) {
+            next(err)
+        }
+    }
+}
+
+module.exports = new AuthController()
