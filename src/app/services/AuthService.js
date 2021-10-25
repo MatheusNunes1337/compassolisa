d90a5158ac570abf7e75b35b503685a603d911e0
Matheus <matheusyagami147@gmail.com>
2021-10-24 23:21:07 -0300

    fix(AuthService): add const response to return the response to controller

--- src/app/services/AuthService.js
+++ src/app/services/AuthService.js
@@ -8,17 +8,19 @@ dotenv.config()
 class AuthService {
     async login(credentials) {
         const user = await AuthRepository.login(credentials)
-        if(!user) {
-            throw new Error('Email or password is incorrent. Try Again')
-        }
 
+        if(!user) 
+            throw new Error('Email or password is incorrent. Try Again')
+    
         const {email, habilitado, _id} = user
 
         const token = jwt.sign({id: _id}, process.env.API_SECRET, {
             expiresIn: '1d'
         })
 
-        return {token, email, habilitado}
+        const response = {token, email, habilitado}
+
+        return response
     }
 } 
 
