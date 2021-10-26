b0592d3eb4871ffb6cebab31599111a83cb3ba87
Matheus <matheusyagami147@gmail.com>
2021-10-26 15:03:48 -0300

    refactor(validations): add conditional to verify if field data_nascimento is in req.body

--- src/app/validations/userValidation.js
+++ src/app/validations/userValidation.js
@@ -6,12 +6,15 @@ const userValidation = async (req, res, next) => {
     try {
         const userData = Object.assign({}, req.body)
         const { reference_date, dob_formated } = convertDateFormat(userData.data_nascimento)
-        userData.data_nascimento = dob_formated
+        
+        if(userData.data_nascimento) {
+            userData.data_nascimento = dob_formated
+        }
         
         const schema = Joi.object({
             nome: Joi.string().when('method', { is: 'POST', then: Joi.required(), otherwise: Joi.optional()}),
             cpf: Joi.string().pattern(new RegExp(/^\d{3}\.\d{3}\.\d{3}\-\d{2}$/)).when('method', { is: 'POST', then: Joi.required(), otherwise: Joi.optional()}),
-            data_nascimento: Joi.date().max(reference_date).when('method', { is: 'POST', then: Joi.required(), otherwise: Joi.optional()}).label('You must be over 18 to create an account'),
+            data_nascimento: Joi.date().max(reference_date).when('method', { is: 'POST', then: Joi.required(), otherwise: Joi.optional()}),
             email: Joi.string().email().when('method', { is: 'POST', then: Joi.required(), otherwise: Joi.optional()}),
             senha: Joi.string().min(6).when('method', { is: 'POST', then: Joi.required(), otherwise: Joi.optional()}),
             habilitado: Joi.string().valid('sim', 'não').when('method', { is: 'POST', then: Joi.required(), otherwise: Joi.optional()})
