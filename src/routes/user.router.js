2e4b45ec1d176563f2265a01e1f0abe06a95e52d
Matheus <matheusyagami147@gmail.com>
2021-10-26 15:02:30 -0300

    feat(routes): add userValidation middleware to put endpoint

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -6,7 +6,7 @@ const checkIdFormat = require('../app/middlewares/checkIdFormat')
 userRouter.get('/', UserController.getAll)
 userRouter.get('/:id', checkIdFormat, UserController.getById)
 userRouter.post('/', userValidation, UserController.create)
-userRouter.put('/:id', UserController.update)
+userRouter.put('/:id', userValidation, UserController.update)
 userRouter.delete('/:id', UserController.delete)
 
 module.exports = userRouter
