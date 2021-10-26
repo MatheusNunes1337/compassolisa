974b79df859a285a794c938a735602d6e1cfbbcf
Matheus <matheusyagami147@gmail.com>
2021-10-26 14:44:08 -0300

    fix(routes): fix userValidation import in user.router.js

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,11 +1,11 @@
 const userRouter = require('express').Router()
 const UserController = require('../app/controllers/UserController')
-const createValidation = require('../app/validations/user/create')
+const userValidation = require('../app/validations/userValidation')
 const checkIdFormat = require('../app/middlewares/checkIdFormat')
 
 userRouter.get('/', UserController.getAll)
 userRouter.get('/:id', checkIdFormat, UserController.getById)
-userRouter.post('/', createValidation, UserController.create)
+userRouter.post('/', userValidation, UserController.create)
 userRouter.put('/:id', UserController.update)
 userRouter.delete('/:id', UserController.delete)
 
