3ffe74c1e9978f95ff4c8105e7286a5fd7be3e13
Matheus <matheusyagami147@gmail.com>
2021-10-27 11:59:03 -0300

    fix(validations): remove Object.assign from userValidation middleware

--- src/app/validations/userValidation.js
+++ src/app/validations/userValidation.js
@@ -1,24 +1,23 @@
 const Joi = require('joi').extend(require('@joi/date'))
-
-const convertDateFormat = require('../utils/convertDateFormat')
+const moment = require('moment')
 
 const userValidation = async (req, res, next) => {
     try {
-        const userData = Object.assign({}, req.body)
-        const { reference_date, dob_formated } = convertDateFormat(userData.data_nascimento)
-        
+        const reference_date  = moment().subtract(18, 'years').format("MM-DD-YYYY")
+       
         const schema = Joi.object({
             nome: Joi.string().when('method', { is: 'POST', then: Joi.required(), otherwise: Joi.optional()}),
             cpf: Joi.string().pattern(new RegExp(/^\d{3}\.\d{3}\.\d{3}\-\d{2}$/)).when('method', { is: 'POST', then: Joi.required(), otherwise: Joi.optional()}),
             data_nascimento: Joi.date()
             .format('DD/MM/YYYY')
             .max(reference_date)
-            .when('method', { is: 'POST', then: Joi.required(), otherwise: Joi.optional()}),
+            .when('method', { is: 'POST', then: Joi.required(), otherwise: Joi.optional()})
+            .label('You must be over 18'),
             email: Joi.string().email().when('method', { is: 'POST', then: Joi.required(), otherwise: Joi.optional()}),
             senha: Joi.string().min(6).when('method', { is: 'POST', then: Joi.required(), otherwise: Joi.optional()}),
             habilitado: Joi.string().valid('sim', 'não').when('method', { is: 'POST', then: Joi.required(), otherwise: Joi.optional()})
         })
-        await schema.validateAsync(userData, { method: req.method }, {abortEarly: true}) 
+        await schema.validateAsync(req.body, { method: req.method }, {abortEarly: true}) 
         return next()    
     } catch(err) {
         return res.status(400).json(err.message)
