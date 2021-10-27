5874ccc51e1bf71a8c3ee86251831575a5ddcc3a
Matheus <matheusyagami147@gmail.com>
2021-10-27 11:07:03 -0300

    feat(validations): add joi/date to userValidation middleware

--- src/app/validations/userValidation.js
+++ src/app/validations/userValidation.js
@@ -1,4 +1,4 @@
-const Joi = require('joi')
+const Joi = require('joi').extend(require('@joi/date'))
 
 const convertDateFormat = require('../utils/convertDateFormat')
 
@@ -7,14 +7,13 @@ const userValidation = async (req, res, next) => {
         const userData = Object.assign({}, req.body)
         const { reference_date, dob_formated } = convertDateFormat(userData.data_nascimento)
         
-        if(userData.data_nascimento) {
-            userData.data_nascimento = dob_formated
-        }
-        
         const schema = Joi.object({
             nome: Joi.string().when('method', { is: 'POST', then: Joi.required(), otherwise: Joi.optional()}),
             cpf: Joi.string().pattern(new RegExp(/^\d{3}\.\d{3}\.\d{3}\-\d{2}$/)).when('method', { is: 'POST', then: Joi.required(), otherwise: Joi.optional()}),
-            data_nascimento: Joi.date().max(reference_date).when('method', { is: 'POST', then: Joi.required(), otherwise: Joi.optional()}),
+            data_nascimento: Joi.date()
+            .format('DD/MM/YYYY')
+            .max(reference_date)
+            .when('method', { is: 'POST', then: Joi.required(), otherwise: Joi.optional()}),
             email: Joi.string().email().when('method', { is: 'POST', then: Joi.required(), otherwise: Joi.optional()}),
             senha: Joi.string().min(6).when('method', { is: 'POST', then: Joi.required(), otherwise: Joi.optional()}),
             habilitado: Joi.string().valid('sim', 'não').when('method', { is: 'POST', then: Joi.required(), otherwise: Joi.optional()})
