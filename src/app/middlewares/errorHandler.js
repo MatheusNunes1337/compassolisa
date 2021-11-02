56e0ee2e409df50dcee5e7431ce3ac975ec89a80
Matheus <matheusyagami147@gmail.com>
2021-11-02 19:45:34 -0300

    feat(middlewares): implement Unauthorized error in errorHandler middleware

--- src/app/middlewares/errorHandler.js
+++ src/app/middlewares/errorHandler.js
@@ -1,4 +1,5 @@
 const NotFound = require('../errors/NotFound');
+const Unauthorized = require('../errors/Unauthorized');
 
 const errorHandler = (err, req, res, next) => {
   let statusCode = 500;
@@ -6,6 +7,8 @@ const errorHandler = (err, req, res, next) => {
   if (err instanceof Error) statusCode = 400;
 
   if (err instanceof NotFound) statusCode = 404;
+
+  if (err instanceof Unauthorized) statusCode = 401;
 
   res.status(statusCode).json({ error: err.message });
 };
