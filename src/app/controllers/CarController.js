a81f372feb9acb6ec11193fc30d85352b5a403c1
Matheus <matheusyagami147@gmail.com>
2021-11-03 20:37:08 -0300

    feat(controllers): implement carSerialize in CarController response methods

--- src/app/controllers/CarController.js
+++ src/app/controllers/CarController.js
@@ -1,10 +1,11 @@
 const CarService = require('../services/CarService');
+const {serialize, paginateSerialize} = require('../serialize/carSerialize')
 
 class CarController {
   async getAll(req, res, next) {
     try {
       const response = await CarService.findAll(req.query);
-      return res.status(200).json(response);
+      return res.status(200).json(paginateSerialize(response));
 
     } catch (err) {
       next(err);
@@ -15,7 +16,7 @@ class CarController {
     try {
       const { id } = req.params;
       const car = await CarService.findById(id);
-      if (car) return res.status(200).json(car);
+      if (car) return res.status(200).json(serialize(car));
       return res.status(204).end();
     } catch (err) {
       next(err);
@@ -25,7 +26,7 @@ class CarController {
   async create(req, res, next) {
     try {
       const car = await CarService.create(req.body);
-      return res.status(201).json(car);
+      return res.status(201).json(serialize(car));
     } catch (err) {
       next(err);
     }
@@ -35,7 +36,7 @@ class CarController {
     try {
       const { id } = req.params;
       const response = await CarService.update(id, req.body);
-      return res.status(200).json(response);
+      return res.status(200).json(serialize(response));
     } catch (err) {
       next(err);
     }
