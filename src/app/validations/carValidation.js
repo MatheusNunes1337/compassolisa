20f72bd098bb6625197a44b3d8844900d27a9de1
Matheus <matheusyagami147@gmail.com>
2021-11-03 19:25:41 -0300

    fix: remove console.log from userValidation

--- src/app/validations/carValidation.js
+++ src/app/validations/carValidation.js
@@ -3,25 +3,25 @@ const Joi = require('joi');
 const carValidation = async (req, res, next) => {
   try {
     const schema = Joi.object({
-      modelo: Joi.string()
+      modelo: Joi.string().trim().min(3)
       .when('method', { is: 'POST', then: Joi.required(), otherwise: Joi.optional() }),
-      cor: Joi.string()
+      cor: Joi.string().trim().min(3)
       .when('method', { is: 'POST', then: Joi.required(), otherwise: Joi.optional() }),
       ano: Joi.number()
       .min(1950)
       .max(2022)
       .when('method', { is: 'POST', then: Joi.required(), otherwise: Joi.optional() }),
       acessorios: Joi.array().min(1).unique('descricao')
-      .when('method', { is: 'POST', then: Joi.required(), otherwise: Joi.optional() })
-      .label('You must to add 1 acessorio at least and they must be unique'),
+      .when('method', { is: 'POST', then: Joi.required(), otherwise: Joi.optional() }),
       quantidadePassageiros: Joi.number()
       .when('method', { is: 'POST', then: Joi.required(), otherwise: Joi.optional() }),
     });
     
-    await schema.validateAsync(req.body, { method: req.method }, { abortEarly: false });
+    const { error } = await schema.validateAsync(req.body, { method: req.method }, { abortEarly: false });
+    if(error) throw error
     return next();
   } catch (err) {
-    return res.status(400).json(err.message);
+    return res.status(400).json(err);
   }
 };
 
