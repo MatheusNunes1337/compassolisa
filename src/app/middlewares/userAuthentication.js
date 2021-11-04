2cb494cd9f230a18934d147bfaaec780c6c03846
Matheus <matheusyagami147@gmail.com>
2021-11-04 11:14:50 -0300

    feat(middlewares): implement errorSerialize and Authentication error in userAuthentication

--- src/app/middlewares/userAuthentication.js
+++ src/app/middlewares/userAuthentication.js
@@ -1,23 +1,26 @@
 const jwt =  require('jsonwebtoken')
 const dotenv = require('dotenv')
 
+const errorSerialize = require('../serialize/errorSerialize')
+const Authentication = require('../errors/Authentication')
+
 dotenv.config()
 
 const userAuthentication = async (req, res, next) => {
   try {
     const authHeader = req.headers.authorization
     if (!authHeader)
-        throw new Error('Token not provided')
+        throw new Authentication('Token not provided')
 
     const parts = authHeader.split(' ')
     if(parts.length !== 2) 
-        throw new Error('The token has an invalid format')
+        throw new Authentication('The token has an invalid format')
 
     const [, token] = parts
     jwt.verify(token, process.env.API_SECRET) 
     next()
   } catch (err) {
-      return res.status(401).json(err.message)
+      return res.status(401).json(errorSerialize(err))
   }
 }
 
