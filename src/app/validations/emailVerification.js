25c3601ed7478d772771a7de9a8c66cc4be77122
Matheus <matheusyagami147@gmail.com>
2021-11-03 21:49:02 -0300

    refactor(validations): replace Error class for Conflict class in emailVerification method

--- src/app/validations/emailVerification.js
+++ src/app/validations/emailVerification.js
@@ -1,9 +1,10 @@
+const Conflict = require('../errors/Conflict')
 const UserModel = require('../models/UserModel');
 
 const emailVerification = async (email) => {
     const user = await UserModel.findOne({email})
     if(user)
-        throw new Error(`Email ${email} already in use`)
+        throw new Conflict(`Email ${email} already in use`)
 };
 
 module.exports = emailVerification;
