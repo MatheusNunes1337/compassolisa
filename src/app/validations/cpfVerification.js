53897c81466559f634e2f0242f07026bcf1e1e9e
Matheus <matheusyagami147@gmail.com>
2021-11-03 21:50:49 -0300

    refactor(validations): replace Error class for Conflict class in cpfVerification method

--- src/app/validations/cpfVerification.js
+++ src/app/validations/cpfVerification.js
@@ -1,9 +1,10 @@
+const Conflict = require('../errors/Conflict')
 const UserModel = require('../models/UserModel');
 
 const cpfVerification = async (cpf) => {
     const user = await UserModel.findOne({cpf})
     if(user)
-        throw new Error(`CPF ${cpf} already in use`)
+        throw new Conflict(`CPF ${cpf} already in use`)
 };
 
 module.exports = cpfVerification;
