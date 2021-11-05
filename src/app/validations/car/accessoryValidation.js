3d96be282bccdb8ededce41c1f99d37f14ef4c5a
Matheus <matheusyagami147@gmail.com>
2021-11-05 16:13:34 -0300

    feat(validations/car): create accessoryValidation middleware

---
+++ src/app/validations/car/accessoryValidation.js
@@ -0,0 +1,23 @@
+const Joi = require('joi');
+const errorSerialize = require('../../serialize/errorSerialize')
+
+const accessoryValidation = async (req, res, next) => {
+  try {
+    const schema = Joi.object({
+      descricao: Joi.string().trim().min(3).required()
+      .messages({
+        "string.empty": `Accessory description is not allowed to be empty`,
+        "string.base": `Accessory description must be a string`,
+        "string.min": `Accessory description length must be at least 3 characters long`,
+      })
+    });
+    
+    const { error } = await schema.validateAsync(req.body, { abortEarly: false });
+    if(error) throw error
+    return next();
+  } catch (err) {
+    return res.status(400).json(errorSerialize(err));
+  }
+};
+
+module.exports = accessoryValidation;
