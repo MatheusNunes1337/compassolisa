77d5a2f6d20d00424b1d796c6f4094c5e3e7f265
Matheus <matheusyagami147@gmail.com>
2021-11-05 16:14:32 -0300

    feat(routes): implement checkIdFormat and accessorryValidation to patch endpoint of car.router

--- src/routes/car.router.js
+++ src/routes/car.router.js
@@ -3,6 +3,7 @@ const carRouter = require('express').Router();
 const CarController = require('../app/controllers/CarController');
 const checkIdFormat = require('../app/middlewares/checkIdFormat');
 const carValidation = require('../app/validations/car/carValidation');
+const accessoryValidation = require('../app/validations/car/accessoryValidation');
 const userAuthentication = require('../app/middlewares/userAuthentication');
 
 carRouter.use(userAuthentication);
@@ -10,7 +11,7 @@ carRouter.get('/', CarController.getAll);
 carRouter.get('/:id', checkIdFormat, CarController.getById);
 carRouter.post('/', carValidation, CarController.create);
 carRouter.put('/:id', checkIdFormat, carValidation, CarController.update);
-carRouter.patch('/:id/acessorios/:accessoryId', CarController.updateAccessory);
+carRouter.patch('/:id/acessorios/:accessoryId', checkIdFormat, accessoryValidation, CarController.updateAccessory);
 carRouter.delete('/:id', checkIdFormat, CarController.delete);
 
 module.exports = carRouter;
