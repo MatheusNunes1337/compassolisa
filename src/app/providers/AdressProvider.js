35e4c36a6d2dce0c8fa4ef845230f7745176cf11
Matheus <matheusyagami147@gmail.com>
2021-11-07 02:24:26 -0300

    feat(routes): add rental routes to routes main file

--- src/app/providers/AdressProvider.js
+++ src/app/providers/AdressProvider.js
@@ -1,18 +1,13 @@
 const axios = require('axios');
-const errorSerialize = require('../serialize/errorSerialize')
 
 class AddressProvider {
     async getAddress(cep) {
-        try {
-            const {data} = await axios.get(`https://viacep.com.br/ws/${cep}/json`)
-            console.log(data)
-        } catch(err) {
-            return res.status(400).json(errorSerialize(err))
-        }
+        const {data} = await axios.get(`https://viacep.com.br/ws/${cep}/json`)
+        console.log(data)
     }
 }
 
 const adressProvider = new AddressProvider()
-adressProvider.getAddress('96055-74000')
+adressProvider.getAddress('96055-740')
 
 //module.exports = new AdressProvider();
