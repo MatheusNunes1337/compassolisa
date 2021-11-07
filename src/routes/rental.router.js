ba7bcbff236f062df79632ef61795880e1776ff7
Matheus <matheusyagami147@gmail.com>
2021-11-07 02:49:08 -0300

    feat(routes): add endpoint to get an rental by its id in rental.router

--- src/routes/rental.router.js
+++ src/routes/rental.router.js
@@ -1,6 +1,8 @@
 const rentalRouter = require('express').Router();
 const RentalController = require('../app/controllers/RentalController');
+const checkIdFormat = require('../app/middlewares/checkIdFormat');
 
 rentalRouter.get('/', RentalController.getAll);
+rentalRouter.get('/:id', checkIdFormat, RentalController.getById);
 
 module.exports = rentalRouter;
