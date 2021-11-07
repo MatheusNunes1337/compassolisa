7a5bf387bccfa1053d6fd39190878644af34ee2d
Matheus <matheusyagami147@gmail.com>
2021-11-07 00:14:54 -0300

    refactor(middlewares): add multiples id validation in checkIdFormat middleware

--- src/app/middlewares/checkIdFormat.js
+++ src/app/middlewares/checkIdFormat.js
@@ -4,17 +4,19 @@ const InvalidParam = require('../errors/InvalidParam')
 
 const checkIdFormat = async (req, res, next) => {
   try {
+    const params = Object.values(req.params)
     const pattern = /[0-9a-fA-F]{24}/;
+
     const schema = Joi.object({
-      id: Joi.string()
-        .regex(pattern)
+      id: Joi.array().items(Joi.string().regex(pattern))
         .messages({
           "string.pattern.base": `id must be 24 hexadecimal numbers`,
         })
-    });
-    const { error } = await schema.validate(req.params, { abortEarly: false });
-    console.log(error)
-    if(error) throw new InvalidParam(error.message)
+      });
+
+    const { error } = await schema.validateAsync({id: params}, { abortEarly: false});
+
+    if(error) throw new error
     return next();
   } catch (err) {
     return res.status(400).json(errorSerialize(err));
