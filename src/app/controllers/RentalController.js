e3d45f8c588db64be63703313d86a23857089b64
Matheus <matheusyagami147@gmail.com>
2021-11-07 02:36:18 -0300

    feat(controllers): add rentalSerialize to response error of getAll method in RentalController

--- src/app/controllers/RentalController.js
+++ src/app/controllers/RentalController.js
@@ -1,10 +1,11 @@
 const RentalService = require('../services/RentalService')
+const {paginateSerialize, serialize} = require('../serialize/rentalSerialize')
 
 class RentalController {
     async getAll(req, res, next) {
         try {
           const response = await RentalService.getAll(req.body)
-          return res.status(200).json(response);
+          return res.status(200).json(paginateSerialize(response));
         } catch (err) {
           next(err);
         }
