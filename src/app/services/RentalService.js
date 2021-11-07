9070cd6ec7d9d83e6588f2f7344afaddbbac7a63
Matheus <matheusyagami147@gmail.com>
2021-11-07 02:48:10 -0300

    feat(services): implement getById method in RentService

--- src/app/services/RentalService.js
+++ src/app/services/RentalService.js
@@ -3,16 +3,20 @@ const BadRequest = require('../errors/BadRequest');
 const NotFound = require('../errors/NotFound');
 
 class RentalService {
-    async getAll({ offset, limit, ...filter }) {
-   
-        offset ? parseInt(offset): undefined;
-        limit ? parseInt(limit) : undefined;
-        
-        if(offset < 0 || limit < 0) 
-          throw new BadRequest('Limit and offset cannot be negative')
-        
-        return await RentalRepository.getAll(offset, limit, filter);
-      }
+  async getAll({ offset, limit, ...filter }) {
+  
+      offset ? parseInt(offset): undefined;
+      limit ? parseInt(limit) : undefined;
+      
+      if(offset < 0 || limit < 0) 
+        throw new BadRequest('Limit and offset cannot be negative')
+      
+      return await RentalRepository.getAll(offset, limit, filter);
+  }
+
+  async getById(id) {
+    return await RentalRepository.getById(id)
+  }
 }
 
 module.exports = new RentalService();
