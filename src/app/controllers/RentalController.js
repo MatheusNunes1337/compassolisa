05a0de933460dec0df5c868e14e62117acbbe262
Matheus <matheusyagami147@gmail.com>
2021-11-08 15:07:48 -0300

    fix(controllers): replace req.body for req.query in getAll method call of RentalController

--- src/app/controllers/RentalController.js
+++ src/app/controllers/RentalController.js
@@ -4,7 +4,7 @@ const {paginateSerialize, serialize} = require('../serialize/rentalSerialize');
 class RentalController {
     async getAll(req, res, next) {
         try {
-          const response = await RentalService.getAll(req.body)
+          const response = await RentalService.getAll(req.query)
           return res.status(200).json(paginateSerialize(response));
         } catch (err) {
           next(err);
