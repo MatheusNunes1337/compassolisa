0080c47a752d17ef44c4f0bc653e6d30cd70c26e
Matheus <matheusyagami147@gmail.com>
2021-11-08 11:49:46 -0300

    feat: implement create method in RentalService

--- src/app/services/RentalService.js
+++ src/app/services/RentalService.js
@@ -1,6 +1,8 @@
 const RentalRepository = require('../repositories/RentalRepository');
+const AddressProvider = require('../providers/AddressProvider')
 const BadRequest = require('../errors/BadRequest');
 const NotFound = require('../errors/NotFound');
+const serialize = require('../serialize/addressSerialize')
 
 class RentalService {
   async getAll({ offset, limit, ...filter }) {
@@ -16,6 +18,32 @@ class RentalService {
 
   async getById(id) {
     return await RentalRepository.getById(id)
+  }
+
+  async create(payload) {
+    const {endereco} = payload
+
+    const matriz = endereco.filter(e => {
+      return e.isFilial === false
+    })
+
+    if(matriz.length === 0) 
+      throw new BadRequest('A rental must have a head office')
+    
+    if(matriz.length > 1) 
+      throw new BadRequest('A rental must have only one head office')
+    
+    payload.endereco = await Promise.all(endereco.map(async e => {
+      const response = await AddressProvider.getAddress(e.cep)
+
+      if(response.hasOwnProperty('erro'))
+        throw new BadRequest(`The cep ${e.cep} does not exist`)
+
+      const fullAddress = Object.assign({}, e, response)
+
+      return serialize(fullAddress, false)
+    }))
+    return await RentalRepository.create(payload)
   }
 
   async delete(id) {
