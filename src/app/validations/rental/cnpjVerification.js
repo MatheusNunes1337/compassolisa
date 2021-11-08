0a50fb841e08aba9b2d7f2a5ef0c8b304086f29d
Matheus <matheusyagami147@gmail.com>
2021-11-08 12:10:29 -0300

    feat(validations/rental): create cnpj verification method

---
+++ src/app/validations/rental/cnpjVerification.js
@@ -0,0 +1,12 @@
+const RentalRepository = require('../../repositories/RentalRepository')
+const {paginateSerialize} = require('../../serialize/rentalSerialize')
+const Conflict = require('../../errors/Conflict')
+
+const cnpjVerification = async (cnpj) => {
+    const rental = await RentalRepository.getAll(0, 100, {cnpj})
+    
+    if(paginateSerialize(rental).locadoras.length > 0)
+      throw new Conflict(`Cnpj ${cnpj} already in use`)
+}
+
+module.exports = cnpjVerification
