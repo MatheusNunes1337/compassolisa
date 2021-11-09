9fca7576203aef6d22020a7aef3c9140284de669
Matheus <matheusyagami147@gmail.com>
2021-11-08 21:06:15 -0300

    fix(providers): add error object inside response of getAddress method in AddressProvider

--- src/app/providers/AddressProvider.js
+++ src/app/providers/AddressProvider.js
@@ -3,7 +3,10 @@ const axios = require('axios');
 class AddressProvider {
     async getAddress(cep) {
         const {data} = await axios.get(`https://viacep.com.br/ws/${cep}/json`)
-        const {logradouro, bairro, localidade, uf} = data
+        const {logradouro, bairro, localidade, uf, erro} = data
+        
+        if(erro)
+            return {erro}
         
         return {logradouro, bairro, localidade, uf}
     }
