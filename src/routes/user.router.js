938270d350343624a16bbe321eb859787e34b25e
Matheus <matheusyagami147@gmail.com>
2021-11-09 12:52:12 -0300

    feat(routes): implement getAll validation in get endpoint of users

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,9 +1,10 @@
 const userRouter = require('express').Router();
 const UserController = require('../app/controllers/UserController');
 const bodyValidation = require('../app/validations/user/createAndUpdate');
+const filterValidation = require('../app/validations/user/getAll');
 const checkIdFormat = require('../app/middlewares/checkIdFormat');
 
-userRouter.get('/', UserController.getAll);
+userRouter.get('/', filterValidation, UserController.getAll);
 userRouter.get('/:id', checkIdFormat, UserController.getById);
 userRouter.post('/', bodyValidation, UserController.create);
 userRouter.put('/:id', checkIdFormat, bodyValidation, UserController.update);
