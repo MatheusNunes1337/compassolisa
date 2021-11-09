f71df11900b24a44b103edb330da5f772d55cd4b
Matheus <matheusyagami147@gmail.com>
2021-11-09 11:55:16 -0300

    feat: add createAndUpdate middleware  in rental post and update endpoints

--- src/routes/rental.router.js
+++ src/routes/rental.router.js
@@ -1,11 +1,12 @@
 const rentalRouter = require('express').Router();
 const RentalController = require('../app/controllers/RentalController');
 const checkIdFormat = require('../app/middlewares/checkIdFormat');
+const createAndUpdate = require('../app/validations/rental/createAndUpdate');
 
 rentalRouter.get('/', RentalController.getAll);
 rentalRouter.get('/:id', checkIdFormat, RentalController.getById);
-rentalRouter.post('/', RentalController.create);
-rentalRouter.put('/:id', checkIdFormat, RentalController.update);
+rentalRouter.post('/', createAndUpdate, RentalController.create);
+rentalRouter.put('/:id', createAndUpdate, checkIdFormat, RentalController.update);
 rentalRouter.delete('/:id', checkIdFormat, RentalController.delete);
 
 module.exports = rentalRouter;
