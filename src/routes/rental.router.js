fc97629e857790d8fd5765a37dea0833e1e23e54
Matheus <matheusyagami147@gmail.com>
2021-11-08 21:10:21 -0300

    feat(routes): add endpoint to update a rental in rental.router

--- src/routes/rental.router.js
+++ src/routes/rental.router.js
@@ -5,6 +5,7 @@ const checkIdFormat = require('../app/middlewares/checkIdFormat');
 rentalRouter.get('/', RentalController.getAll);
 rentalRouter.get('/:id', checkIdFormat, RentalController.getById);
 rentalRouter.post('/', RentalController.create);
+rentalRouter.put('/:id', checkIdFormat, RentalController.update);
 rentalRouter.delete('/:id', checkIdFormat, RentalController.delete);
 
 module.exports = rentalRouter;
