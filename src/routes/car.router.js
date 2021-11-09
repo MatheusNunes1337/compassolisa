6b683dca5c5b199c01ca25dd6fa3d1fdd0a38743
Matheus <matheusyagami147@gmail.com>
2021-11-09 12:55:06 -0300

    fix(routes): fix import of createAndUpdate validation in car.router

--- src/routes/car.router.js
+++ src/routes/car.router.js
@@ -2,15 +2,15 @@ const carRouter = require('express').Router();
 
 const CarController = require('../app/controllers/CarController');
 const checkIdFormat = require('../app/middlewares/checkIdFormat');
-const carValidation = require('../app/validations/car/carValidation');
+const bodyValidation = require('../app/validations/car/createAndUpdate');
 const accessoryValidation = require('../app/validations/car/accessoryValidation');
 const userAuthentication = require('../app/middlewares/userAuthentication');
 
 carRouter.use(userAuthentication);
 carRouter.get('/', CarController.getAll);
 carRouter.get('/:id', checkIdFormat, CarController.getById);
-carRouter.post('/', carValidation, CarController.create);
-carRouter.put('/:id', checkIdFormat, carValidation, CarController.update);
+carRouter.post('/', bodyValidation, CarController.create);
+carRouter.put('/:id', checkIdFormat, bodyValidation, CarController.update);
 carRouter.patch('/:id/acessorios/:accessoryId', checkIdFormat, accessoryValidation, CarController.updateAccessory);
 carRouter.delete('/:id', checkIdFormat, CarController.delete);
 
