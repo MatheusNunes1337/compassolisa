84154b3e9047da83997294fe5aa9b90ee603bbd4
Matheus <matheusyagami147@gmail.com>
2021-11-10 12:59:58 -0300

    feat(infra/database): config databaseUrl to tests

--- src/infra/database/index.js
+++ src/infra/database/index.js
@@ -2,6 +2,13 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config();
+let databaseUrl;
+
+if (process.env.NODE_ENV === 'test') {
+  databaseUrl = 'mongodb://localhost:27017/test';
+} else {
+  databaseUrl = process.env.MONGO_URL;
+}
 
 class DatabaseConnection {
   constructor() {
@@ -9,7 +16,7 @@ class DatabaseConnection {
   }
 
   connect() {
-    mongoose.connect(process.env.MONGO_URL);
+    mongoose.connect(databaseUrl);
   }
 }
 
