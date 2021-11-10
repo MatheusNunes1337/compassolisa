27308df3f7d9acee80453b9fa47581cfc44138f0
Matheus <matheusyagami147@gmail.com>
2021-11-10 10:22:10 -0300

    feat(validations/rental): rename addressValidation file to addressExistence

---
+++ src/app/validations/rental/addressExistence.js
@@ -0,0 +1,16 @@
+const RentalRepository = require('../../repositories/RentalRepository')
+const {paginateSerialize} = require('../../serialize/rentalSerialize')
+const Conflict = require('../../errors/Conflict')
+
+const checkAddressExistence = async (cep, number) => {
+    const filter = {}
+    filter['endereco.cep'] = cep
+    filter['endereco.number'] = number
+
+    const rental = await RentalRepository.getAll(0, 100, filter)
+    
+    if(paginateSerialize(rental).locadoras.length > 0)
+      throw new Conflict(`The address with cep ${cep} and number ${number} already in use`)
+}
+
+module.exports = checkAddressExistence
