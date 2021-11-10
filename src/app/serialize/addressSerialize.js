87596dc2534d0323d98cc1f5257ea26b00470b9f
Matheus <matheusyagami147@gmail.com>
2021-11-10 11:02:44 -0300

    fix: remove omitIsFilial parameter from addressSerialize

--- src/app/serialize/addressSerialize.js
+++ src/app/serialize/addressSerialize.js
@@ -1,14 +1,7 @@
 const serialize = ({
   cep, number, complemento, isFilial, logradouro, bairro, localidade, uf,
-}, omitIsFilial = true) => {
-  if (omitIsFilial) {
-    return {
-      cep, logradouro, complemento, bairro, number, localidade, uf,
-    };
-  }
-  return {
-    cep, logradouro, complemento, bairro, number, localidade, uf, isFilial,
-  };
-};
+}) => ({
+  cep, logradouro, complemento, bairro, number, localidade, uf, isFilial,
+});
 
 module.exports = serialize;
