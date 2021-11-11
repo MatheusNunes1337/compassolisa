dd28c6eb931025529957643d1f5da1cf888165b8
Matheus <matheusyagami147@gmail.com>
2021-11-11 20:28:51 -0300

    feat(routes): implement create and update car schema validations to car.router as middleware

--- src/routes/car.router.js
+++ src/routes/car.router.js
@@ -2,7 +2,8 @@ const carRouter = require('express').Router();
 
 const CarController = require('../app/controllers/CarController');
 const checkIdFormat = require('../app/middlewares/checkIdFormat');
-const bodyValidation = require('../app/validations/car/createAndUpdate');
+const createCar = require('../app/validations/car/createCar');
+const updateCar = require('../app/validations/car/updateCar');
 const filterValidation = require('../app/validations/car/getAll');
 const accessoryValidation = require('../app/validations/car/accessoryValidation');
 const userAuthentication = require('../app/middlewares/userAuthentication');
@@ -10,8 +11,8 @@ const userAuthentication = require('../app/middlewares/userAuthentication');
 carRouter.use(userAuthentication);
 carRouter.get('/', filterValidation, CarController.getAll);
 carRouter.get('/:id', checkIdFormat, CarController.getById);
-carRouter.post('/', bodyValidation, CarController.create);
-carRouter.put('/:id', checkIdFormat, bodyValidation, CarController.update);
+carRouter.post('/', createCar, CarController.create);
+carRouter.put('/:id', checkIdFormat, updateCar, CarController.update);
 carRouter.patch('/:id/acessorios/:accessoryId', checkIdFormat, accessoryValidation, CarController.updateAccessory);
 carRouter.delete('/:id', checkIdFormat, CarController.delete);
 
