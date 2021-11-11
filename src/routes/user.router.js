0a840c0184a5591a78432e43b7d40bd3a248183d
Matheus <matheusyagami147@gmail.com>
2021-11-11 20:47:12 -0300

    feat(routes): implement createUser and updateUser schemas in post and put endpoints of user.router

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,13 +1,14 @@
 const userRouter = require('express').Router();
 const UserController = require('../app/controllers/UserController');
-const bodyValidation = require('../app/validations/user/createAndUpdate');
+const createUser = require('../app/validations/user/createUser');
+const updateUser = require('../app/validations/user/updateUser');
 const filterValidation = require('../app/validations/user/getAll');
 const checkIdFormat = require('../app/middlewares/checkIdFormat');
 
 userRouter.get('/', filterValidation, UserController.getAll);
 userRouter.get('/:id', checkIdFormat, UserController.getById);
-userRouter.post('/', bodyValidation, UserController.create);
-userRouter.put('/:id', checkIdFormat, bodyValidation, UserController.update);
+userRouter.post('/', createUser, UserController.create);
+userRouter.put('/:id', checkIdFormat, updateUser, UserController.update);
 userRouter.delete('/:id', checkIdFormat, UserController.delete);
 
 module.exports = userRouter;
