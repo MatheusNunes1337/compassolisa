7cffa07c6ff4567048f358e18155bdf10882ba1d
Matheus <matheusyagami147@gmail.com>
2021-11-11 21:26:32 -0300

    feat(routes): add createRental and updateRental to post and put endpoints of rental.router

--- src/routes/rental.router.js
+++ src/routes/rental.router.js
@@ -1,13 +1,14 @@
 const rentalRouter = require('express').Router();
 const RentalController = require('../app/controllers/RentalController');
 const checkIdFormat = require('../app/middlewares/checkIdFormat');
-const bodyValidation = require('../app/validations/rental/createAndUpdate');
+const createRental = require('../app/validations/rental/createRental');
+const updateRental = require('../app/validations/rental/updateRental');
 const filterValidation = require('../app/validations/rental/getAll');
 
 rentalRouter.get('/', filterValidation, RentalController.getAll);
 rentalRouter.get('/:id', checkIdFormat, RentalController.getById);
-rentalRouter.post('/', bodyValidation, RentalController.create);
-rentalRouter.put('/:id', checkIdFormat, bodyValidation, RentalController.update);
+rentalRouter.post('/', createRental, RentalController.create);
+rentalRouter.put('/:id', checkIdFormat, updateRental, RentalController.update);
 rentalRouter.delete('/:id', checkIdFormat, RentalController.delete);
 
 module.exports = rentalRouter;
