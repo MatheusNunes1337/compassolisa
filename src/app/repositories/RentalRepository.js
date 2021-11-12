49e4690e3a553d67ec554826a9b1e2d72b354850
Matheus <matheusyagami147@gmail.com>
2021-11-12 03:33:48 -0300

    style(repositories): fix eslint errors from RentalRepository

--- src/app/repositories/RentalRepository.js
+++ src/app/repositories/RentalRepository.js
@@ -1,25 +1,25 @@
 const RentalModel = require('../models/RentalModel');
 
 class RentalRepository {
-    async getAll(offset = 0, limit = 100, filter) {
-        return await RentalModel.paginate(filter, { offset, limit })
-    }
+  async getAll(filter, offset = 0, limit = 100) {
+    return RentalModel.paginate(filter, { offset, limit });
+  }
 
-    async getById(id) {
-        return await RentalModel.findById(id)
-    }
+  async getById(id) {
+    return RentalModel.findById(id);
+  }
 
-    async create(payload) {
-        return await RentalModel.create(payload)
-    }
+  async create(payload) {
+    return RentalModel.create(payload);
+  }
 
-    async update(id, payload) {
-       return await RentalModel.findByIdAndUpdate(id, payload, { new: true });
-    }
+  async update(id, payload) {
+    return RentalModel.findByIdAndUpdate(id, payload, { new: true });
+  }
 
-    async delete(id) {
-        return await RentalModel.findByIdAndDelete(id)
-    }
+  async delete(id) {
+    return RentalModel.findByIdAndDelete(id);
+  }
 }
 
 module.exports = new RentalRepository();
