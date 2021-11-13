1846a8f619b3fac1805c1f52fa701e4a854d411b
Matheus <matheusyagami147@gmail.com>
2021-11-12 21:11:47 -0300

    test: move mock objects to beforeEach of each rental test

--- __tests__/feature/rental.test.js
+++ __tests__/feature/rental.test.js
@@ -11,9 +11,14 @@ beforeEach(async () => {
   await RentalModel.deleteMany();
 });
 
+let rentalMock = {};
+const rentalMock02 = {};
+
 describe('create a new rental', () => {
-  it('should return status code 201', async () => {
-    const rentalMock = {
+  beforeEach(async () => {
+    await RentalModel.deleteMany();
+
+    rentalMock = {
       nome: 'Locadora do mock',
       cnpj: '16.760.085/0920-10',
       atividades: 'Alugel de carros e gestão de frotas',
@@ -26,7 +31,9 @@ describe('create a new rental', () => {
         }
       ]
     };
+  });
 
+  it('should return status code 201', async () => {
     const response = await request(app).post('/api/v1/rental/').send(rentalMock);
 
     const { status } = response;
@@ -34,20 +41,6 @@ describe('create a new rental', () => {
   });
 
   it('should return a body with _id and the same properties from rentalMock with full address except isFilial', async () => {
-    const rentalMock = {
-      nome: 'Locadora do mock',
-      cnpj: '16.760.085/0920-10',
-      atividades: 'Alugel de carros e gestão de frotas',
-      endereco: [
-        {
-          cep: '96055-740',
-          number: 201,
-          complemento: 'ao lado da havan',
-          isFilial: false
-        }
-      ]
-    };
-
     const response = await request(app).post('/api/v1/rental/').send(rentalMock);
 
     const { body } = response;
@@ -67,20 +60,6 @@ describe('create a new rental', () => {
   });
 
   it('should return a body with values type string, except number(number) and isFilial(undefined)', async () => {
-    const rentalMock = {
-      nome: 'Locadora do mock',
-      cnpj: '16.760.085/0920-10',
-      atividades: 'Alugel de carros e gestão de frotas',
-      endereco: [
-        {
-          cep: '96055-740',
-          number: 201,
-          complemento: 'ao lado da havan',
-          isFilial: false
-        }
-      ]
-    };
-
     const response = await request(app).post('/api/v1/rental/').send(rentalMock);
 
     const { body } = response;
@@ -98,8 +77,10 @@ describe('create a new rental', () => {
 });
 
 describe('Do not create a rental with cep that not exists', () => {
-  it('should return status code 400', async () => {
-    const rentalMock = {
+  beforeEach(async () => {
+    await RentalModel.deleteMany();
+
+    rentalMock = {
       nome: 'Locadora do mock',
       cnpj: '16.760.085/0920-10',
       atividades: 'Alugel de carros e gestão de frotas',
@@ -112,7 +93,9 @@ describe('Do not create a rental with cep that not exists', () => {
         }
       ]
     };
+  });
 
+  it('should return status code 400', async () => {
     const response = await request(app).post('/api/v1/rental/').send(rentalMock);
 
     const { status } = response;
@@ -120,20 +103,6 @@ describe('Do not create a rental with cep that not exists', () => {
   });
 
   it('should return a body with name and description error properties', async () => {
-    const rentalMock = {
-      nome: 'Locadora do mock',
-      cnpj: '16.760.085/0920-10',
-      atividades: 'Alugel de carros e gestão de frotas',
-      endereco: [
-        {
-          cep: '12455-000',
-          number: 201,
-          complemento: 'ao lado da havan',
-          isFilial: false
-        }
-      ]
-    };
-
     const response = await request(app).post('/api/v1/rental/').send(rentalMock);
 
     const { body } = response;
@@ -143,90 +112,6 @@ describe('Do not create a rental with cep that not exists', () => {
   });
 
   it('should return a body with values type string', async () => {
-    const rentalMock = {
-      nome: 'Locadora do mock',
-      cnpj: '16.760.085/0920-10',
-      atividades: 'Alugel de carros e gestão de frotas',
-      endereco: [
-        {
-          cep: '12455-000',
-          number: 201,
-          complemento: 'ao lado da havan',
-          isFilial: false
-        }
-      ]
-    };
-
-    const response = await request(app).post('/api/v1/rental/').send(rentalMock);
-
-    const { body } = response;
-
-    expect(typeof body).toBe('object');
-    expect(typeof body.name).toBe('string');
-    expect(typeof body.description).toBe('string');
-  });
-});
-
-describe('Do not create a rental with cep that not exists', () => {
-  it('should return status code 400', async () => {
-    const rentalMock = {
-      nome: 'Locadora do mock',
-      cnpj: '16.760.085/0920-10',
-      atividades: 'Alugel de carros e gestão de frotas',
-      endereco: [
-        {
-          cep: '12455-000',
-          number: 201,
-          complemento: 'ao lado da havan',
-          isFilial: false
-        }
-      ]
-    };
-
-    const response = await request(app).post('/api/v1/rental/').send(rentalMock);
-
-    const { status } = response;
-    expect(status).toBe(400);
-  });
-
-  it('should return a body with name and description error properties', async () => {
-    const rentalMock = {
-      nome: 'Locadora do mock',
-      cnpj: '16.760.085/0920-10',
-      atividades: 'Alugel de carros e gestão de frotas',
-      endereco: [
-        {
-          cep: '12455-000',
-          number: 201,
-          complemento: 'ao lado da havan',
-          isFilial: false
-        }
-      ]
-    };
-
-    const response = await request(app).post('/api/v1/rental/').send(rentalMock);
-
-    const { body } = response;
-
-    expect(body.name).toBe(`The cep ${rentalMock.endereco[0].cep} does not exist`);
-    expect(body.description).toBe('Bad Request');
-  });
-
-  it('should return a body with values type string', async () => {
-    const rentalMock = {
-      nome: 'Locadora do mock',
-      cnpj: '16.760.085/0920-10',
-      atividades: 'Alugel de carros e gestão de frotas',
-      endereco: [
-        {
-          cep: '12455-000',
-          number: 201,
-          complemento: 'ao lado da havan',
-          isFilial: false
-        }
-      ]
-    };
-
     const response = await request(app).post('/api/v1/rental/').send(rentalMock);
 
     const { body } = response;
@@ -238,8 +123,10 @@ describe('Do not create a rental with cep that not exists', () => {
 });
 
 describe('Do not create a rental with no head office', () => {
-  it('should return status code 400', async () => {
-    const rentalMock = {
+  beforeEach(async () => {
+    await RentalModel.deleteMany();
+
+    rentalMock = {
       nome: 'Compassolisa',
       cnpj: '18.172.927/0920-15',
       atividades: 'Alugel de carros',
@@ -252,7 +139,9 @@ describe('Do not create a rental with no head office', () => {
         }
       ]
     };
+  });
 
+  it('should return status code 400', async () => {
     const response = await request(app).post('/api/v1/rental/').send(rentalMock);
 
     const { status } = response;
@@ -260,20 +149,6 @@ describe('Do not create a rental with no head office', () => {
   });
 
   it('should return a body with name and description error properties', async () => {
-    const rentalMock = {
-      nome: 'Compassolisa',
-      cnpj: '18.172.927/0920-15',
-      atividades: 'Alugel de carros',
-      endereco: [
-        {
-          cep: '12455-000',
-          number: 201,
-          complemento: 'ao lado da havan',
-          isFilial: true
-        }
-      ]
-    };
-
     const response = await request(app).post('/api/v1/rental/').send(rentalMock);
 
     const { body } = response;
@@ -283,20 +158,6 @@ describe('Do not create a rental with no head office', () => {
   });
 
   it('should return a body with values type string', async () => {
-    const rentalMock = {
-      nome: 'Compassolisa',
-      cnpj: '18.172.927/0920-15',
-      atividades: 'Alugel de carros',
-      endereco: [
-        {
-          cep: '12455-000',
-          number: 201,
-          complemento: 'ao lado da havan',
-          isFilial: true
-        }
-      ]
-    };
-
     const response = await request(app).post('/api/v1/rental/').send(rentalMock);
 
     const { body } = response;
@@ -308,8 +169,10 @@ describe('Do not create a rental with no head office', () => {
 });
 
 describe('Do not create a rental with duplicated adresses', () => {
-  it('should return status code 400', async () => {
-    const rentalMock = {
+  beforeEach(async () => {
+    await RentalModel.deleteMany();
+
+    rentalMock = {
       nome: 'Locadora 01',
       cnpj: '16.123.213/7291-19',
       atividades: 'Aluguel de carros de luxo',
@@ -328,7 +191,9 @@ describe('Do not create a rental with duplicated adresses', () => {
         }
       ]
     };
+  });
 
+  it('should return status code 400', async () => {
     const response = await request(app).post('/api/v1/rental/').send(rentalMock);
 
     const { status } = response;
@@ -336,26 +201,6 @@ describe('Do not create a rental with duplicated adresses', () => {
   });
 
   it('should return a body with name and description error properties', async () => {
-    const rentalMock = {
-      nome: 'Locadora 01',
-      cnpj: '16.123.213/7291-19',
-      atividades: 'Aluguel de carros de luxo',
-      endereco: [
-        {
-          cep: '96055-740',
-          number: 745,
-          complemento: 'ao lado da havan',
-          isFilial: true
-        },
-        {
-          cep: '96055-740',
-          number: 745,
-          complemento: 'ao lado da havan',
-          isFilial: false
-        }
-      ]
-    };
-
     const response = await request(app).post('/api/v1/rental/').send(rentalMock);
 
     const { body } = response;
@@ -367,26 +212,6 @@ describe('Do not create a rental with duplicated adresses', () => {
   });
 
   it('should return a body with values type string', async () => {
-    const rentalMock = {
-      nome: 'Locadora 01',
-      cnpj: '16.123.213/7291-19',
-      atividades: 'Aluguel de carros de luxo',
-      endereco: [
-        {
-          cep: '96055-740',
-          number: 745,
-          complemento: 'ao lado da havan',
-          isFilial: true
-        },
-        {
-          cep: '96055-740',
-          number: 745,
-          complemento: 'ao lado da havan',
-          isFilial: false
-        }
-      ]
-    };
-
     const response = await request(app).post('/api/v1/rental/').send(rentalMock);
 
     const { body } = response;
@@ -398,8 +223,10 @@ describe('Do not create a rental with duplicated adresses', () => {
 });
 
 describe('delete a rental', () => {
-  it('should return status code 204', async () => {
-    const rentalMock = {
+  beforeEach(async () => {
+    await RentalModel.deleteMany();
+
+    rentalMock = {
       nome: 'Moonlight',
       cnpj: '12.567.124/1039-11',
       atividades: 'Aluguel de ferraris',
@@ -418,7 +245,9 @@ describe('delete a rental', () => {
         }
       ]
     };
+  });
 
+  it('should return status code 204', async () => {
     const { text } = await request(app).post('/api/v1/rental/').send(rentalMock);
 
     const { _id } = JSON.parse(text);
@@ -430,26 +259,6 @@ describe('delete a rental', () => {
   });
 
   it('should return a empty object', async () => {
-    const rentalMock = {
-      nome: 'Moonlight',
-      cnpj: '12.567.124/1039-11',
-      atividades: 'Aluguel de ferraris',
-      endereco: [
-        {
-          cep: '20050-000',
-          number: 201,
-          complemento: '',
-          isFilial: false
-        },
-        {
-          cep: '20050-000',
-          number: 467,
-          complemento: 'Na frente da padaria do Jorge',
-          isFilial: true
-        }
-      ]
-    };
-
     const { text } = await request(app).post('/api/v1/rental/').send(rentalMock);
 
     const { _id } = JSON.parse(text);
@@ -465,26 +274,6 @@ describe('delete a rental', () => {
   });
 
   it('should return a body type object', async () => {
-    const rentalMock = {
-      nome: 'Moonlight',
-      cnpj: '12.567.124/1039-11',
-      atividades: 'Aluguel de ferraris',
-      endereco: [
-        {
-          cep: '20050-000',
-          number: 201,
-          complemento: '',
-          isFilial: false
-        },
-        {
-          cep: '20050-000',
-          number: 467,
-          complemento: 'Na frente da padaria do Jorge',
-          isFilial: true
-        }
-      ]
-    };
-
     const { text } = await request(app).post('/api/v1/rental/').send(rentalMock);
 
     const { _id } = JSON.parse(text);
