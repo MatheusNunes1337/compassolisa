8a7c9b9547031cd7839f20e0a908e604189b9e96
Matheus <matheusyagami147@gmail.com>
2021-11-16 13:09:22 -0300

    feat: refactor createUser.test

---
+++ __tests__/feature/user/createUser.test.js
@@ -0,0 +1,228 @@
+const request = require('supertest');
+
+const app = require('../../../src/index');
+const UserModel = require('../../../src/app/models/UserModel');
+
+beforeAll(async () => {
+  await UserModel.deleteMany();
+});
+
+beforeEach(async () => {
+  await UserModel.deleteMany();
+});
+
+let userMock = {};
+let userMock02 = {};
+
+describe('create a new user', () => {
+  beforeEach(() => {
+    userMock = {
+      nome: 'James winston',
+      cpf: '182.931.371-08',
+      data_nascimento: '17/12/1945',
+      email: 'james1945@outlook.com',
+      senha: 'mynae13',
+      habilitado: 'não'
+    };
+  });
+
+  it('should return status code 201', async () => {
+    const response = await request(app).post('/api/v1/people/').send(userMock);
+
+    const { status } = response;
+    expect(status).toBe(201);
+  });
+
+  it('should return a body with _id and the same properties from userMock expect password', async () => {
+    const { body } = await request(app).post('/api/v1/people/').send(userMock);
+
+    expect(body).toHaveProperty('_id');
+    expect(body.nome).toBe(userMock.nome);
+    expect(body.cpf).toBe(userMock.cpf);
+    expect(body.data_nascimento).toBe(userMock.data_nascimento);
+    expect(body.email).toBe(userMock.email);
+    expect(body.habilitado).toBe(userMock.habilitado);
+  });
+
+  it('should return a body with values type string', async () => {
+    const { body } = await request(app).post('/api/v1/people/').send(userMock);
+
+    expect(body).toEqual({
+      _id: expect.any(String),
+      nome: expect.any(String),
+      cpf: expect.any(String),
+      data_nascimento: expect.any(String),
+      email: expect.any(String),
+      habilitado: expect.any(String)
+    });
+  });
+});
+
+describe('Do not create a user with a existing email', () => {
+  beforeEach(() => {
+    userMock = {
+      nome: 'Bruna Garcia',
+      cpf: '391.385.123-05',
+      data_nascimento: '11/10/1978',
+      email: 'bruna@outlook.com',
+      senha: '123456',
+      habilitado: 'não'
+    };
+
+    userMock02 = {
+      nome: 'Bruna Bustamante',
+      cpf: '982.123.281-07',
+      data_nascimento: '12/12/2000',
+      email: 'bruna@outlook.com',
+      senha: 'mm84na9',
+      habilitado: 'sim'
+    };
+  });
+  it('should return status code 400', async () => {
+    await request(app).post('/api/v1/people/').send(userMock);
+
+    const { status } = await request(app).post('/api/v1/people/').send(userMock02);
+
+    expect(status).toBe(400);
+  });
+
+  it('should return a body with description and name properties', async () => {
+    await request(app).post('/api/v1/people/').send(userMock);
+
+    const { body } = await request(app).post('/api/v1/people/').send(userMock02);
+
+    expect(body.description).toBe('Conflict');
+    expect(body.name).toBe(`Email ${userMock02.email} already in use`);
+  });
+
+  it('should return a body with values type string', async () => {
+    await request(app).post('/api/v1/people/').send(userMock);
+
+    const { body } = await request(app).post('/api/v1/people/').send(userMock02);
+
+    expect(body).toEqual({
+      description: expect.any(String),
+      name: expect.any(String)
+    });
+  });
+});
+
+describe('Do not not create a user with a existing cpf', () => {
+  beforeEach(() => {
+    userMock = {
+      nome: 'Cristina',
+      cpf: '391.382.102-01',
+      data_nascimento: '13/12/1987',
+      email: 'cristina083@outlook.com',
+      senha: '123bga',
+      habilitado: 'não'
+    };
+
+    userMock02 = {
+      nome: 'Bruno Henrique',
+      cpf: '391.382.102-01',
+      data_nascimento: '12/12/1999',
+      email: 'brunohenrique@outlook.com',
+      senha: 'mm84na9',
+      habilitado: 'sim'
+    };
+  });
+
+  it('should return status code 400', async () => {
+    await request(app).post('/api/v1/people/').send(userMock);
+
+    const { status } = await request(app).post('/api/v1/people/').send(userMock02);
+
+    expect(status).toBe(400);
+  });
+
+  it('should return a body with description and name properties', async () => {
+    await request(app).post('/api/v1/people/').send(userMock);
+
+    const { body } = await request(app).post('/api/v1/people/').send(userMock02);
+
+    expect(body.description).toBe('Conflict');
+    expect(body.name).toBe(`CPF ${userMock.cpf} already in use`);
+  });
+
+  it('should return a body with values type string', async () => {
+    await request(app).post('/api/v1/people/').send(userMock);
+
+    const { body } = await request(app).post('/api/v1/people/').send(userMock02);
+
+    expect(body).toEqual({
+      description: expect.any(String),
+      name: expect.any(String)
+    });
+  });
+});
+
+describe('Do not create a user with invalid email format', () => {
+  beforeEach(() => {
+    userMock = {
+      nome: 'Roger Guedes',
+      cpf: '347.189.193-07',
+      data_nascimento: '27/10/1978',
+      email: 'roguerg',
+      senha: 'lavista6381',
+      habilitado: 'sim'
+    };
+  });
+
+  it('should return status code 400', async () => {
+    const { status } = await request(app).post('/api/v1/people/').send(userMock);
+
+    expect(status).toBe(400);
+  });
+
+  it('should return a body with description and name properties', async () => {
+    const { body } = await request(app).post('/api/v1/people/').send(userMock);
+
+    expect(body[0].description).toBe('email');
+    expect(body[0].name).toBe('"email" must be a valid email');
+  });
+
+  it('should return a body with values type string', async () => {
+    const { body } = await request(app).post('/api/v1/people/').send(userMock);
+
+    expect(body[0]).toEqual({
+      description: expect.any(String),
+      name: expect.any(String)
+    });
+  });
+});
+
+describe('Do not create a user with blank field', () => {
+  beforeEach(() => {
+    userMock = {
+      nome: '',
+      cpf: '892.182.109-02',
+      data_nascimento: '29/10/2002',
+      email: 'marina2022@outlook.com',
+      senha: '123456',
+      habilitado: 'sim'
+    };
+  });
+
+  it('should return status code 400', async () => {
+    const { status } = await request(app).post('/api/v1/people/').send(userMock);
+
+    expect(status).toBe(400);
+  });
+
+  it('should return a body with description and name properties', async () => {
+    const { body } = await request(app).post('/api/v1/people/').send(userMock);
+
+    expect(body[0].description).toBe('nome');
+    expect(body[0].name).toBe('"nome" is not allowed to be empty');
+  });
+
+  it('should return a body with values type string', async () => {
+    const { body } = await request(app).post('/api/v1/people/').send(userMock);
+
+    expect(body[0]).toEqual({
+      description: expect.any(String),
+      name: expect.any(String)
+    });
+  });
+});
