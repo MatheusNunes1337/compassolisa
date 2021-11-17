850eebeed0c3abdf9786f620d36fb2c193d7e610
Matheus <matheusyagami147@gmail.com>
2021-11-17 00:54:44 -0300

    feat(serialize): add verification to addressSerialize to hide isFilial field

--- src/app/serialize/addressSerialize.js
+++ src/app/serialize/addressSerialize.js
@@ -1,12 +1,16 @@
-const serialize = ({ cep, number, complemento, isFilial, logradouro, bairro, localidade, uf }) => ({
-  cep,
-  logradouro,
-  complemento,
-  bairro,
-  number,
-  localidade,
-  uf,
-  isFilial
-});
+const serialize = (
+  { cep, number, complemento, isFilial, logradouro, bairro, localidade, uf },
+  hideIsFilial = false
+) => {
+  let objectSerialized;
+
+  if (hideIsFilial === false) {
+    objectSerialized = { cep, logradouro, complemento, bairro, number, localidade, uf, isFilial };
+  } else {
+    objectSerialized = { cep, logradouro, complemento, bairro, number, localidade, uf };
+  }
+
+  return objectSerialized;
+};
 
 module.exports = serialize;
