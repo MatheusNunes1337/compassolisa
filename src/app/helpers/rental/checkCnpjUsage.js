fd7b2c21f1ef33555189932d952559b9f386136f
Matheus <matheusyagami147@gmail.com>
2021-11-16 23:57:43 -0300

    fix(helpers): sort params order (filter, limit, offfset) in getAll method call in checkCnpjUsage

--- src/app/helpers/rental/checkCnpjUsage.js
+++ src/app/helpers/rental/checkCnpjUsage.js
@@ -1,11 +1,9 @@
 const RentalRepository = require('../../repositories/RentalRepository');
-const { paginateSerialize } = require('../../serialize/rentalSerialize');
 const Conflict = require('../../errors/Conflict');
 
 const checkCnpjUsage = async (cnpj) => {
-  const rental = await RentalRepository.getAll(0, 100, { cnpj });
-
-  if (paginateSerialize(rental).locadoras.length > 0) throw new Conflict(`Cnpj ${cnpj} already in use`);
+  const rental = await RentalRepository.getAll({ cnpj }, 0, 100);
+  if (rental.docs.length > 0) throw new Conflict(`Cnpj ${cnpj} already in use`);
 };
 
 module.exports = checkCnpjUsage;
