dccb7b0a8a40c3a68e8133464792edc5b48e471a
Matheus <matheusyagami147@gmail.com>
2021-11-18 13:45:57 -0300

    chore: enable coveragePathIgnorePatterns field in jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -26,9 +26,7 @@ module.exports = {
   coverageDirectory: '__tests__/coverage',
 
   // An array of regexp pattern strings used to skip coverage collection
-  // coveragePathIgnorePatterns: [
-  //   "/node_modules/"
-  // ],
+  coveragePathIgnorePatterns: ['/node_modules/', '/src/server.js'],
 
   // Indicates which provider should be used to instrument code for coverage
   // coverageProvider: 'v8',
