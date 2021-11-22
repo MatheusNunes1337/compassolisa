8a9d52600bf82fb8d2def7126a322acad1d22d18
Matheus <matheusyagami147@gmail.com>
2021-11-22 11:46:45 -0300

    feat(routes): add fleet router to routes main file

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,11 +3,13 @@ const routes = require('express').Router();
 const userRouter = require('./user.router');
 const carRouter = require('./car.router');
 const rentalRouter = require('./rental.router');
+const fleetRouter = require('./fleet.router');
 const authRouter = require('./auth.router');
 
 routes.use('/api/v1/people', userRouter);
 routes.use('/api/v1/car', carRouter);
 routes.use('/api/v1/rental', rentalRouter);
+routes.use('/api/v1/rental/:id/fleet', fleetRouter);
 routes.use('/api/v1/authenticate', authRouter);
 
 module.exports = routes;
