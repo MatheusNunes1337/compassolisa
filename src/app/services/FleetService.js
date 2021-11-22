d18db72cf33c505ba0257d07a39b6b07fe4fdbad
Matheus <matheusyagami147@gmail.com>
2021-11-22 12:51:03 -0300

    feat(services): create getById method in fleet service

--- src/app/services/FleetService.js
+++ src/app/services/FleetService.js
@@ -3,12 +3,19 @@ const RentalRepository = require('../repositories/RentalRepository');
 const NotFound = require('../errors/NotFound');
 
 class FleetService {
-  async getAll({ offset, limit, ...filter }, { id }) {
-    const rental = await RentalRepository.getById(id);
+  async getAll({ offset, limit, ...filter }, { rentalId }) {
+    const rental = await RentalRepository.getById(rentalId);
     if (!rental) throw new NotFound('Rental');
 
-    filter.id_locadora = id;
+    filter.id_locadora = rentalId;
     return FleetRepository.getAll(filter, offset, limit);
+  }
+
+  async getById({ id, rentalId }) {
+    const rental = await RentalRepository.getById(rentalId);
+    if (!rental) throw new NotFound('Rental');
+
+    return FleetRepository.getById(id, rentalId);
   }
 }
 
