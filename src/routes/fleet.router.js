a3185e93d898732e8bc030680ad390c96258b4c4
Matheus Nunes <matheusyagami147@gmail.com>
2021-11-24 14:17:35 -0300

    feat: implement get all and update fleet schema validations to put and get all endpoints of fleet

--- src/routes/fleet.router.js
+++ src/routes/fleet.router.js
@@ -3,12 +3,14 @@ const fleetRouter = require('express').Router({ mergeParams: true });
 const FleetController = require('../app/controllers/FleetController');
 const checkIdFormat = require('../app/middlewares/checkIdFormat');
 const createFleet = require('../app/validations/fleet/createFleet');
+const updateFleet = require('../app/validations/fleet/updateFleet');
+const getAll = require('../app/validations/fleet/getAll');
 
 fleetRouter.use(checkIdFormat);
-fleetRouter.get('/', FleetController.getAll);
+fleetRouter.get('/', getAll, FleetController.getAll);
 fleetRouter.get('/:id', FleetController.getById);
 fleetRouter.post('/', createFleet, FleetController.create);
-fleetRouter.put('/:id', FleetController.update);
+fleetRouter.put('/:id', updateFleet, FleetController.update);
 fleetRouter.delete('/:id', FleetController.delete);
 
 module.exports = fleetRouter;
