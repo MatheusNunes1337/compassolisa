1d3ebefb764ce27fab8fc0bd949c9e35e41a07f1
Matheus Nunes <matheusyagami147@gmail.com>
2021-11-26 11:45:31 -0300

    feat(routes): add endpoint to get a reserve by Id in reserve.router

--- src/routes/reserve.router.js
+++ src/routes/reserve.router.js
@@ -4,5 +4,6 @@ const ReserveController = require('../app/controllers/ReserveController');
 const checkIdFormat = require('../app/middlewares/checkIdFormat');
 
 reserveRouter.get('/', checkIdFormat, ReserveController.getAll);
+reserveRouter.get('/:id', checkIdFormat, ReserveController.getById);
 
 module.exports = reserveRouter;
