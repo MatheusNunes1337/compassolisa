42f532b7770089e581dc34d7af5dfa8fcfa6a3b3
Matheus Nunes <matheusyagami147@gmail.com>
2021-11-26 12:10:20 -0300

    feat(routes): add authentication middleware to reserve router

--- src/routes/reserve.router.js
+++ src/routes/reserve.router.js
@@ -2,7 +2,9 @@ const reserveRouter = require('express').Router({ mergeParams: true });
 
 const ReserveController = require('../app/controllers/ReserveController');
 const checkIdFormat = require('../app/middlewares/checkIdFormat');
+const userAuthentication = require('../app/middlewares/userAuthentication');
 
+reserveRouter.use(userAuthentication);
 reserveRouter.get('/', checkIdFormat, ReserveController.getAll);
 reserveRouter.get('/:id', checkIdFormat, ReserveController.getById);
 
