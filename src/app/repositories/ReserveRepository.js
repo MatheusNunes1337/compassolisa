034091db59bd85c82dc7d35c70cfa1350c2df82e
Matheus Nunes <matheusyagami147@gmail.com>
2021-11-26 22:37:02 -0300

    feat(repositories): add getById method in ReserveRepository

--- src/app/repositories/ReserveRepository.js
+++ src/app/repositories/ReserveRepository.js
@@ -5,6 +5,10 @@ class ReserveRepository extends Repository {
   constructor() {
     super(ReserveSchema);
   }
+
+  async getById(_id, id_locadora) {
+    return ReserveSchema.findById({ _id, id_locadora });
+  }
 }
 
 module.exports = new ReserveRepository();
