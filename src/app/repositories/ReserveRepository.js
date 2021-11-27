acb3a7f7167021774d0e4f00ba62dedbe0e35f4e
Matheus Nunes <matheusyagami147@gmail.com>
2021-11-26 22:44:31 -0300

    feat(repository): implement delete method in ReserveRepository

--- src/app/repositories/ReserveRepository.js
+++ src/app/repositories/ReserveRepository.js
@@ -9,6 +9,10 @@ class ReserveRepository extends Repository {
   async getById(_id, id_locadora) {
     return ReserveSchema.findById({ _id, id_locadora });
   }
+
+  async delete(_id, id_locadora) {
+    return ReserveSchema.findByIdAndDelete({ _id, id_locadora });
+  }
 }
 
 module.exports = new ReserveRepository();
