434aa2ca0024efb1c6702d850353f51453422c24
Matheus Nunes <matheusyagami147@gmail.com>
2021-11-28 21:51:20 -0300

    refactor(utils): return just the regex instead of create a variable pattern in getPattern methods

--- src/app/utils/getPatterns.js
+++ src/app/utils/getPatterns.js
@@ -1,27 +1,12 @@
-const getCnpjPattern = () => {
-  const pattern = /^\d{2}\.\d{3}\.\d{3}\/\d{4}-\d{2}$/;
-  return pattern;
-};
+const getCnpjPattern = () => /^\d{2}\.\d{3}\.\d{3}\/\d{4}-\d{2}$/;
 
-const getCepPattern = () => {
-  const pattern = /^\d{5}-\d{3}$/;
-  return pattern;
-};
+const getCepPattern = () => /^\d{5}-\d{3}$/;
 
-const getCpfPattern = () => {
-  const pattern = /^\d{3}\.\d{3}\.\d{3}-\d{2}$/;
-  return pattern;
-};
+const getCpfPattern = () => /^\d{3}\.\d{3}\.\d{3}-\d{2}$/;
 
-const getObjectIdPattern = () => {
-  const pattern = /[0-9a-fA-F]{24}/;
-  return pattern;
-};
+const getObjectIdPattern = () => /[0-9a-fA-F]{24}/;
 
-const getLicensePlatePattern = () => {
-  const pattern = /[A-Z]{3}[0-9][0-9A-Z][0-9]{2}/;
-  return pattern;
-};
+const getLicensePlatePattern = () => /[A-Z]{3}[0-9][0-9A-Z][0-9]{2}/;
 
 module.exports = {
   getCnpjPattern,
